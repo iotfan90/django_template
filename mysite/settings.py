70e9796e0cabb672d349d12d8a0254bec17c9e63
iotfan90 <iotfan90@gmail.com>
2020-11-23 12:29:03 +0200

    Add production db setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,12 +89,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gambling-shrink-dev",
+        "USER": "gambling_shrink",
+        "PASSWORD": "nwiJj25P%UbisxT",
+        "HOST": "gambling-shrink-dev.cakbgvx8q9gj.ap-southeast-2.rds.amazonaws.com",
+        "PORT": "5432",
+    },
 }
+
 
 
 # Password validation
