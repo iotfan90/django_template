ca44da7e6123fffe0c40fd4472c5e9207397e1f3
iotfan90 <iotfan90@gmail.com>
2020-11-25 22:34:42 +0200

    Add django builtin admin page

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,22 +91,22 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "gambling_shrink_dev",
-        "USER": "gambling_shrink",
-        "PASSWORD": "nwiJj25P%UbisxT",
-        "HOST": "gambling-shrink-dev.cakbgvx8q9gj.ap-southeast-2.rds.amazonaws.com",
-        "PORT": "5432",
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "gambling_shrink_dev",
+#         "USER": "gambling_shrink",
+#         "PASSWORD": "nwiJj25P%UbisxT",
+#         "HOST": "gambling-shrink-dev.cakbgvx8q9gj.ap-southeast-2.rds.amazonaws.com",
+#         "PORT": "5432",
+#     },
+# }
 
 
 
@@ -128,6 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SITE_ID = 1
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
